668ca6ca065d84676aecf47ca32cd56c4a10ccbd
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-15 10:24:42 +0530

    button container and display module css file added

--- Calc-version1/src/App.jsx
+++ Calc-version1/src/App.jsx
@@ -1,15 +1,12 @@
 import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
   return (
       <div className= {styles.calculator} >
-        <input type="text" className= {styles.display}/>
-        <div className= {styles.buttonsContainer}>
-          <button className={styles.button}>C</button>
-          <button className={styles.button}>1</button>
-          <button className={styles.button}>2</button>
-          <button className={styles.button}>+</button>
-        </div>
+        <Display></Display>
+        <ButtonsContainer></ButtonsContainer>
       </div>
   );
 }
